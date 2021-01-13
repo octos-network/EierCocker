150a00c27d457485b13569285ef9b694cf5b8973
octos-network <octochicken7aber3@gmail.com>
2021-01-13 08:23:40 +0100

    SingleDeviceNotes

---
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.java
@@ -0,0 +1,23 @@
+package com.example.android.eggtimernotifications;
+
+import android.app.Service;
+
+class MyFirebaseMessagingService : FireBaseMessagingService() {
+
+        // TODO: Step 3.2 log registration token
+        // [START on_new_token]
+        /**
+         * Called if InstanceID token is updated. This may occur if the security of
+         * the previous token had been compromised. Note that this is called when the InstanceID token
+         * is initially generated so this is where you would retrieve the token.
+         */
+        override fun onNewToken(token: String?) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token)
+        }
+        // [END on_new_token]
+}
