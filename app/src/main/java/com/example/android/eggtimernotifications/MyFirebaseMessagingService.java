068e9ae0a3224ad4f4e3e08575f8a0753b0e2182
octos-network <octochicken7aber3@gmail.com>
2021-01-13 10:06:32 +0100

    Daten Massage

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.java
@@ -1,23 +1,42 @@
 package com.example.android.eggtimernotifications;
 
 import android.app.Service;
+import android.util.Log;
 
-class MyFirebaseMessagingService : FireBaseMessagingService() {
+import com.google.firebase.messaging.RemoteMessage;
 
-        // TODO: Step 3.2 log registration token
-        // [START on_new_token]
-        /**
-         * Called if InstanceID token is updated. This may occur if the security of
-         * the previous token had been compromised. Note that this is called when the InstanceID token
-         * is initially generated so this is where you would retrieve the token.
-         */
-        override fun onNewToken(token: String?) {
+import static android.content.ContentValues.TAG;
+
+public class MyFirebaseMessagingService extends Service {
+
+    // [START receive_message]
+    override fun onMessageReceived(remoteMessage:RemoteMessage?) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage?.from}")
+
+    // TODO: Step 3.5 check messages for data
+    // Check if message contains a data payload.
+    remoteMessage?.data?.let {
+        Log.d(TAG, "Message data payload: " + remoteMessage.data)
+    }
+
+}
+// [END receive_message]
+
+    // TODO: Step 3.2 log registration token
+    // [START on_new_token]
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String?) {
         Log.d(TAG, "Refreshed token: $token")
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
         sendRegistrationToServer(token)
-        }
-        // [END on_new_token]
+    }
+    // [END on_new_token]
 }
